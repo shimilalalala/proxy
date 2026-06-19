68f7d44714e0c51ef118a56800d358c8231366c8
lawr3nceBill <lbmhalili2@gmail.com>
2026-06-19 22:37:32 +0400

    Restructure Netlify function as directory bundle

--- netlify/functions/api.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
-
-from mangum import Mangum
-from main import app
-
-handler = Mangum(app, lifespan="off")
